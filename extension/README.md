9417c4c70da6de2d0aa2c0da1b306c3573ef0fe6
Johan Backman <johan.backman@brkt.com>
2016-08-09 09:42:59 +0200

    :tada: Project structure

---
+++ extension/README.md
@@ -0,0 +1,8 @@
+# Chrome GMail Festival Extension
+Lorem hack ipsum
+
+## Load extension locally
+1. Go to chrome://extensions/
+2. Activate "Developer Mode"
+3. Click on load unpacked extension and select the repository folder
+4. Done!
---
+++ extension/content.js
@@ -0,0 +1,11 @@
+var j = document.createElement('script');
+j.src = chrome.extension.getURL('js/jquery-1.9.1.min.js');
+(document.head || document.documentElement).appendChild(j);
+
+var g = document.createElement('script');
+g.src = chrome.extension.getURL('js/gmail.js');
+(document.head || document.documentElement).appendChild(g);
+
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('src/main.js');
+(document.head || document.documentElement).appendChild(s);
