d50f3690e49e42d3fb0f2d448ea53c57f6373c58
Erik Öskog <erik.oskog@gmail.com>
2016-08-09 19:32:43 +0200

    Added updated scripts

--- extension/content.js
+++ extension/content.js
@@ -11,9 +11,13 @@ s.src = chrome.extension.getURL('src/main.js');
 (document.head || document.documentElement).appendChild(s);
 
 var q = document.createElement('script');
-s.src = chrome.extension.getURL('js/bootstrap.min.js');
+q.src = chrome.extension.getURL('js/bootstrap.min.js');
 (document.head || document.documentElement).appendChild(q);
 
 var p = document.createElement('script');
-s.src = chrome.extension.getURL('js/handlebars.min.js');
+p.src = chrome.extension.getURL('js/handlebars.min.js');
 (document.head || document.documentElement).appendChild(p);
+
+var r = document.createElement('script');
+r.src = chrome.extension.getURL('js/src/options/options.js');
+(document.head || document.documentElement).appendChild(r);
