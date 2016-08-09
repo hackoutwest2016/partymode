9534aa09ee46558f21be8b29174a1217f9f35940
Johan Backman <johan.backman@brkt.com>
2016-08-09 12:04:33 +0200

    :construction: Add crude gmail sidebar widget

--- extension/content.js
+++ extension/content.js
@@ -3,7 +3,7 @@ j.src = chrome.extension.getURL('js/jquery-1.9.1.min.js');
 (document.head || document.documentElement).appendChild(j);
 
 var g = document.createElement('script');
-g.src = chrome.extension.getURL('js/gmail.js');
+g.src = chrome.extension.getURL('js/inboxsdk.js');
 (document.head || document.documentElement).appendChild(g);
 
 var s = document.createElement('script');
--- extension/src/main.js
+++ extension/src/main.js
@@ -1,16 +1,33 @@
-var gmail;
 
-function refresh(f) {
-    if( (/in/.test(document.readyState)) || (typeof Gmail === undefined) ) {
-        setTimeout('refresh(' + f + ')', 10);
-    } else {
-        f();
-    }
+InboxSDK.load('1.0', 'sdk_partyhardhow16_d05fc23638').then(function(sdk){
+
+    // the SDK has been loaded, now do something with it!
+    sdk.Compose.registerComposeViewHandler(function(composeView){
+
+    // a compose view has come into existence, do something with it!
+    composeView.addButton({
+            title: "My Nifty Button!",
+            iconUrl: 'https://example.com/foo.png',
+            onClick: function(event) {
+                event.composeView.insertTextIntoBodyAtCursor('Hello World!');
+            },
+        });
+    });
+
+    sdk.Conversations.registerMessageViewHandler(function(messageView) {
+        var threadView = messageView.getThreadView();
+        addSidebar(threadView);
+    });
+});
+
+function addSidebar(threadView) {
+    var el = document.createElement("div");
+    el.innerHTML = 'Hello world!';
+
+    threadView.addSidebarContentPanel({
+        el: el,
+        title: "PARTY HARD",
+        iconUrl: chrome.runtime.getURL('img/coachella.png')
+    });
 }
 
-var main = function(){
-    gmail = new Gmail();
-    console.log('Hello,', gmail.get.user_email())
-}
-
-refresh(main);
